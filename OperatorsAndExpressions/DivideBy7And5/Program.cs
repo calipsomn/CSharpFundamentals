282047e65cfcd672cb7dfb8a5bd53483591a5cc9
calipsomn <calipsomn@yahoo.com>
2016-04-04 00:03:40 +0300

    Divide by 7 and 5 and rectangles

---
+++ OperatorsAndExpressions/DivideBy7And5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideBy7And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(number % 7 == 0 && number % 5 == 0 ? "true " : "false " + number);
+        }
+    }
+}
---
+++ OperatorsAndExpressions/Rectangles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width = float.Parse(Console.ReadLine());
+            float height = float.Parse(Console.ReadLine());
+            Console.WriteLine((width * height).ToString("0.00") + " " + (2 * width + 2 * height).ToString("0.00"));
+        }
+    }
+}
