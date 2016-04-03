8fc94291df5d76d4452cde82f45c3e2e65c7fbad
calipsomn <calipsomn@yahoo.com>
2016-04-03 23:25:03 +0300

    13 and 14 task

---
+++ DataTypesAndVariables/PrintTheASCIITable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintTheASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 128; i++)
+            {
+                char ch = (char)i;
+                if (char.IsControl(ch))
+                    continue;
+
+                Console.Write(ch + " ");
+            }
+        }
+    }
+}
