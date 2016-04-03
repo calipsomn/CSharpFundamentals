2fecc37348d9cf4a5ca1409c7afca643f2b74bce
calipsomn <calipsomn@yahoo.com>
2016-04-03 23:53:12 +0300

    Operator and expressions

---
+++ DataTypesAndVariables/ComparingFloats/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double double1 = double.Parse(Console.ReadLine());
+            double double2 = double.Parse(Console.ReadLine());
+            double difference = Math.Abs(double1 - double2);
+            Console.WriteLine(difference < 0.000001 ? "true" : "false");
+        }
+    }
+}
---
+++ OperatorsAndExpressions/MoonGravity/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonGravity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float w = float.Parse(Console.ReadLine());
+            Console.WriteLine((w * 17.00 / 100.00).ToString("0.000"));
+        }
+    }
+}
