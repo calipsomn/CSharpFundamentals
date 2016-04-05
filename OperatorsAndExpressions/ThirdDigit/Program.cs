a6c37a93bc5f597f54b6bca1503f401eb14120ed
Eli Velichkova <calipsomn@yahoo.com>
2016-04-05 15:36:00 +0300

    Third digit solution

---
+++ OperatorsAndExpressions/ThirdDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var thirdDigit = n.ToString("D3").Reverse<char>().ToArray()[2];
+            Console.WriteLine(thirdDigit == '7' ? "true" : "false " + thirdDigit);
+        }
+    }
+}
